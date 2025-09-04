c6ce27d3eb0cb8b12b8b0164e69ee93574a6736a
CAWL ecommerce <ecommerce-integrations@cawl.com>
2025-09-04 10:48:33 +0000

    Push plugin cawl version 2.5.0

--- cawl.php
+++ cawl.php
@@ -4,7 +4,7 @@
  * Plugin Name: CAWL
  * Description: CAWL.
  * Version:     2.5.0
- * SHA:        8ebe251
+ * SHA:        3a6d986
  * Requires at least: 6.3
  * Requires PHP: 7.4
  * Requires Plugins: woocommerce
